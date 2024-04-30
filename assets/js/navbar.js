842e644612f837a5c565ee2a1bd220c7d0e5dda0
malde k <malt.ketten@gmail.com>
2024-04-30 21:19:05 +0200

    navbar js

---
+++ assets/js/navbar.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).on('scroll', function () {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+          $('.site-navigation').addClass('nav-bg');
+          $('#logo-icon').attr('src', 'images/favicon.png');
+          $('.navbar-brand').css('color', 'black');
+          $('.nav-link').css('color', 'black');
+      
+          $(".nav-link").mouseover(function() {
+            $(this).css("color","#00b4ff");
+          }).mouseout(function() {
+            $(this).css("color","black");
+          });
+        
+        } else {
+          $('.site-navigation').removeClass('nav-bg');
+          $('#logo-icon').attr('src', 'images/logo.png');
+          $('.navbar-brand').css('color', 'white');
+          $('.nav-link').css('color', 'white');
+      
+          $(".nav-link").mouseover(function() {
+            $(this).css("color","#00b4ff");
+          }).mouseout(function() {
+            $(this).css("color","#fff");
+          });
+        }
+      });
+});
