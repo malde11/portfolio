223d5c956f31531ef9eb2fb1ecd657acc121bd0b
malde k <malt.ketten@gmail.com>
2024-05-02 16:53:10 +0200

    still navigation

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,7 @@
-$(document).ready(function() {
+'use strict';
 
-    "use strict";
+$(document).ready(function () {
 
-    $('.site-navigation').css('background', 'black');
-    
     $(window).on('scroll', function () {
         var windscroll = $(window).scrollTop();
         if (windscroll >= 100) {
@@ -31,4 +29,26 @@ $(document).ready(function() {
           });
         }
       });
+
+    
+/* Video */
+
+    const mainVideo = $('#myVideo');
+    
+    const medi      = "videos/ground_540p.mp4";
+    const large     = "videos/ground_1080p.mp4";
+
+    switch ( true ) {
+
+        case ($(window).width() >= 1080):
+            mainVideo.append("<source type='video/mp4' src='" + large + "' />");
+            break;
+
+        case ($(window).width() >= 720):
+            mainVideo.append("<source type='video/mp4' src='" + medi + "' />");
+            break;
+    
+    }
+      
+
 });
