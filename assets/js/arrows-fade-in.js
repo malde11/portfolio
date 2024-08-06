6129f159955379fdc29f44ead45d435a0c88c261
malte k <malt.ketten@gmail.com>
2024-08-06 19:35:40 +0200

    playing with arrwos

--- assets/js/arrows-fade-in.js
+++ assets/js/arrows-fade-in.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-// Arrows function        
-$(".arrows-fade").delay(10500).fadeIn("slow");
+    // Arrows function        
+    $(".arrows-fade").delay(10000).show()
 
 });
