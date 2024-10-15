e6d12a30e178c713ee13d69558839c8ca33f31ad
malte k <malt.ketten@gmail.com>
2024-10-15 17:12:25 +0200

    hover navbar

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -7,10 +7,19 @@ function scrollFunction() {
 
   console.log("nav scroll");
 
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    $(".navbar").css("background-color", "black");
+  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+    $(".navbar").css("background-color", "white");
+    $(".navbar-brand").css("color", "black");
+    $(".nav-link").css("color", "black");
+    $(".brand-logo").css("display", "none");
+    $(".brand-name").css("margin-left", "35px");
+
   } else {
     $(".navbar").css("background-color", "black");
+    $(".navbar-brand").css("color", "white");
+    $(".nav-link").css("color", "white");
+    $(".brand-logo").css("display", "inline");
+    $(".brand-name").css("margin-left", "0px");
   }
 
 }
