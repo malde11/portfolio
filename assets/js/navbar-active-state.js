27305ba7f642870b3b67d62b62de2ceee9afd050
malde k <malt.ketten@gmail.com>
2024-11-29 17:16:49 +0100

    remove prelader.. and remove startseite

--- assets/js/navbar-active-state.js
+++ assets/js/navbar-active-state.js
@@ -4,8 +4,8 @@ console.log("active-nav");
 
 pathname = window.location.pathname;
 
-pages = [' ', 'logos', 'sketches', 'fotos', 'tutorials', 'kontakt'];
-pagesen = [' ', 'logos', 'sketches', 'photos', 'tutorials', 'contact'];
+pages = ['logos', 'sketches', 'fotos', 'tutorials', 'kontakt'];
+pagesen = ['logos', 'sketches', 'photos', 'tutorials', 'contact'];
 
 $('.nav-link').each(function(i) {
     if (pathname.includes(pages[i]) || pathname.includes(pagesen[i])) $(this).addClass('active');
